0b296b29b4f715174589b61a99132786fe74abed
“ypeed” <patra.kaca@gmail.com>
2021-04-02 00:41:07 +0200

    configure tailwindcss custom colors

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,6 +2,18 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      inputColor: '#E6E6E6',
+      blue: {
+        light: '#03779E',
+        DEFAULT: '#032A4E',
+      },
+      green: '#158800',
+      red: '#D13A26',
+      gray: '#8195A7'
+    },
     extend: {},
   },
   variants: {
