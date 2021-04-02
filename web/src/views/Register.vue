bbe2dac3646e59e8a9dd91b33df7af8b45abb7be
tomas <tomas.kovacic1@gmail.com>
2021-04-02 15:13:57 +0200

    Register design done

--- web/src/views/Register.vue
+++ web/src/views/Register.vue
@@ -3,45 +3,40 @@
     <div class="flex items-center justify-center bg-blue w-full">
       <form class="rounded-3xl bg-white shadow-login w-1/2">
         <div class="text-center">
-          <h1
-            class="font-customFont text-5xl font-semibold mt-6 ml-12 mr-12"
-          >
+          <h1 class="font-customFont text-5xl font-semibold mt-6 ml-12 mr-12">
             Create account
           </h1>
-          <h1
-            class="font-customFont text-base mt-2 mb-10 ml-12 mr-12"
-          >
-            Already have an account? <router-link :to="{ name: 'Login'}" class="text-red">Sign in</router-link>
+          <h1 class="font-customFont text-base mt-2 mb-10 ml-12 mr-12">
+            Already have an account?
+            <router-link :to="{ name: 'Login' }" class="text-red"
+              >Sign in</router-link
+            >
           </h1>
         </div>
-        <div class="flex flex-col">
-          <div class="inline-flex">
-            <div class="flex-1">
-              <input
-                name="firstname"
-                type="text"
-                autocomplete="firstname"
-                required
-                class="pl-6 py-2 mx-2 mb-4 font-light font-customFont text-xl placeholder-gray-loginInputText bg-gray-loginInputBg rounded-full"
-                placeholder="First Name"
-              />
-            </div>
+        <div class="flex flex-row justify-center mx-12">
 
-            <div class="flex-1">
-              <input
-                name="lastname"
-                type="text"
-                autocomplete="lastname"
-                required
-                class="pl-6 py-2 mx-2 font-light font-customFont text-xl placeholder-gray-loginInputText bg-gray-loginInputBg rounded-full"
-                placeholder="Last Name"
-              />
-            </div>
-          </div>
+            <input
+              name="firstname"
+              type="text"
+              autocomplete="firstname"
+              required
+              class="mr-0.5 w-1/2 pl-6 py-2 mb-4 font-light font-customFont text-xl placeholder-gray-loginInputText bg-gray-loginInputBg rounded-l-full"
+              placeholder="First Name"
+            />
+
+            <input
+              name="lastname"
+              type="text"
+              autocomplete="lastname"
+              required
+              class="ml-0.5 w-1/2 pl-6 py-2 mb-4 font-light font-customFont text-xl placeholder-gray-loginInputText bg-gray-loginInputBg rounded-r-full"
+              placeholder="Last Name"
+            />
+        </div>
+        <div class="flex flex-col">
           <input
             name="username"
             type="text"
-            autocomplete="username"
             required
             class="pl-6 py-2 mx-12 mb-4 font-light font-customFont text-xl placeholder-gray-loginInputText bg-gray-loginInputBg rounded-full"
             placeholder="Username"
@@ -74,15 +69,14 @@
         </div>
       </form>
     </div>
-    <div class="flex-wrap px-3 sm:max-w-2xl mx-8">
+    <div class="flex flex-col px-3 sm:max-w-2xl mx-8">
       <div>
-        <img src="../assets/register.svg" class="w-2/3 h-2/3 ml-40" />
+        <img src="../assets/register.svg" class="flex w-10/12 h-10/12 ml-10 pt-24" />
       </div>
-      <div class="my-12 ml-10 .w-64">
-        <h1 class="text-6xl">Solve-x TimeTracker</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod
+      <div class="my-12 ml-4 .w-80 pt-14	">
+        <h1 class="text-6xl font-customFont">Solve-x TimeTracker</h1>
+        <p class="font-customFont ">
+          Sign up and start managing your time
         </p>
       </div>
     </div>
