b5455f102ea867a8e6f5bfaa991d28bc4d281e2e
tomas <tomas.kovacic1@gmail.com>
2021-04-06 11:18:02 +0200

    Navbar component added

---
+++ web/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+        <div class="absolute flex top-0 h-screen bg-blue">
+      <div
+        class="bg-yellow-200 overflow-hidden flex items-center justify-center"
+      >
+        <div class="w-48 text-center font-bold text-xl text-white">Navigation</div>
+      </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="fixed inset-0 flex z-40">
-    <div class="absolute flex top-0 h-screen bg-blue">
-      <div
-        class="bg-yellow-200 overflow-hidden flex items-center justify-center"
-      >
-        <div class="w-48 text-center font-bold text-xl text-white">Navigation</div>
-      </div>
-    </div>
+    <div class="fixed inset-0 flex z-40">
+      <Navbar></Navbar>
 
     <div class="flex-1 bg-gray-400 bg-opacity-75" />
   </div>
 </template>
+
+<script>
+import Navbar from '../components/Navbar'
+
+export default {
+  data(){
+
+  },
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
