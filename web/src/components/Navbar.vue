3c7b05862eaf176ee0afcc102c430e3e03d79a05
“ypeed” <patra.kaca@gmail.com>
2021-04-06 12:07:15 +0200

    created new branch

--- web/src/components/Navbar.vue
+++ web/src/components/Navbar.vue
@@ -1,17 +1,15 @@
 <template>
-        <div class="absolute flex top-0 h-screen bg-blue">
-      <div
-        class="bg-yellow-200 overflow-hidden flex items-center justify-center"
-      >
-        <div class="w-48 text-center font-bold text-xl text-white">Navigation</div>
+  <div class="absolute flex top-0 h-screen bg-blue">
+    <div class="bg-yellow-200 overflow-hidden flex items-center justify-center">
+      <div class="w-48 text-center font-bold text-xl text-white">
+        Navigationnnn
       </div>
     </div>
+  </div>
 </template>
 
 <script>
-
 </script>
 
 <style>
-
 </style>
