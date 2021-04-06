edd468471c66decb6149ef0d1ee3442da0272312
tomas <tomas.kovacic1@gmail.com>
2021-04-06 12:12:59 +0200

    test branch

--- web/src/components/Navbar.vue
+++ web/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="absolute flex top-0 h-screen bg-blue">
     <div class="bg-yellow-200 overflow-hidden flex items-center justify-center">
       <div class="w-48 text-center font-bold text-xl text-white">
-        Navigationnnn
+        Navigation
       </div>
     </div>
   </div>
