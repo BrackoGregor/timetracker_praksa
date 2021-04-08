3083f98ab31aa3f68c69894592567c936147ab1b
tomas <tomas.kovacic1@gmail.com>
2021-04-08 14:58:36 +0200

    Navbar simplified

--- web/src/components/Navbar.vue
+++ web/src/components/Navbar.vue
@@ -11,132 +11,18 @@
         </a>
         <div class="mt-20">
           <ul>
-            <li
-              class="mb-6"
-              v-on:click="
-                (isClicked = false),
-                  (isClicked2 = true),
-                  (isClicked3 = true),
-                  (isClicked4 = true),
-                  (isClicked5 = true)
-              "
-            >
-              <router-link :to="{ name: 'Add' }">
+            <li class="mb-6" v-for="link in links" :key="link.id" >
+              <router-link :to="{ name: link.route }" >
                 <span>
                   <img
-                    src="../assets/add.svg"
-                    class="rounded-full w-5 h-5 mb-3 mx-auto opacity-50"
-                    v-show="isClicked"
-                  />
-                  <img
-                    src="../assets/add_solid.svg"
+                    :src="link.img1"
                     class="rounded-full w-5 h-5 mb-3 mx-auto"
-                    v-show="!isClicked"
-                  />
-                </span>
-              </router-link>
-            </li>
-            <li
-              class="mb-6"
-              v-on:click="
-                (isClicked = true),
-                  (isClicked2 = false),
-                  (isClicked3 = true),
-                  (isClicked4 = true),
-                  (isClicked5 = true)
-              "
-            >
-              <router-link :to="{ name: 'Projects' }">
-                <span>
-                  <img
-                    src="../assets/project.svg"
-                    class="rounded-full w-5 h-5 mb-3 mx-auto opacity-50"
-                    v-show="isClicked2"
-                  />
-                  <img
-                    src="../assets/project_solid.svg"
-                    class="rounded-full w-5 h-5 mb-3 mx-auto"
-                    v-show="!isClicked2"
-                  />
-                </span>
-              </router-link>
-            </li>
-            <li
-              class="mb-6"
-              v-on:click="
-                (isClicked = true),
-                  (isClicked2 = true),
-                  (isClicked3 = false),
-                  (isClicked4 = true),
-                  (isClicked5 = true)
-              "
-            >
-              <router-link :to="{ name: 'Clients' }">
-                <span>
-                  <img
-                    src="../assets/client.svg"
-                    class="rounded-full w-5 h-5 mb-3 mx-auto opacity-50"
-                    v-show="isClicked3"
-                  />
-                  <img
-                    src="../assets/client_solid.svg"
-                    class="rounded-full w-5 h-5 mb-3 mx-auto"
-                    v-show="!isClicked3"
-                  />
-                </span>
-              </router-link>
-            </li>
-            <li
-              v-on:click="
-                (isClicked = true),
-                  (isClicked2 = true),
-                  (isClicked3 = true),
-                  (isClicked4 = false),
-                  (isClicked5 = true)
-              "
-            >
-              <router-link :to="{ name: 'Calendar' }">
-                <span>
-                  <img
-                    src="../assets/calendar.svg"
-                    class="rounded-full w-5 h-5 mb-3 mx-auto opacity-50"
-                    v-show="isClicked4"
-                  />
-                  <img
-                    src="../assets/calendar_solid.svg"
-                    class="rounded-full w-5 h-5 mb-3 mx-auto"
-                    v-show="!isClicked4"
                   />
                 </span>
               </router-link>
             </li>
           </ul>
         </div>
-      </div>
-      <div
-        class="mb-5"
-        v-on:click="
-          (isClicked = true),
-            (isClicked2 = true),
-            (isClicked3 = true),
-            (isClicked4 = true),
-            (isClicked5 = false)
-        "
-      >
-        <router-link :to="{ name: 'Settings' }">
-          <span>
-            <img
-              src="../assets/settings.svg"
-              class="rounded-full w-5 h-5 mb-3 mx-auto opacity-50"
-              v-show="isClicked5"
-            />
-            <img
-              src="../assets/settings_solid.svg"
-              class="rounded-full w-5 h-5 mb-3 mx-auto"
-              v-show="!isClicked5"
-            />
-          </span>
-        </router-link>
       </div>
     </nav>
     <nav
@@ -160,156 +46,23 @@
                     src="../assets/person.svg"
                     class="rounded-full w-12 h-12 mb-3 mr-0.5 ml-4 border-solid border-2 border-white"
                   />
-                  <h1 class="text-white mt-2 ml-4 mr-4 text-2xl font-customFont ">
+                  <h1
+                    class="text-white mt-2 ml-4 mr-4 text-2xl font-customFont"
+                  >
                     Tomas Kovačič
                   </h1>
                 </span>
               </div>
             </li>
-            <li
-              class="mb-2"
-              v-on:click="
-                (isClicked = false),
-                  (isClicked2 = true),
-                  (isClicked3 = true),
-                  (isClicked4 = true),
-                  (isClicked5 = true)
-              "
-            >
-              <router-link :to="{ name: 'Add' }">
+            <li class="mb-2" v-for="link in links" :key="link.id">
+              <router-link :to="{ name: link.route }" class="router-link-exact-active">
                 <span class="flex">
                   <img
-                    src="../assets/add.svg"
-                    class="rounded-full w-5 h-5 mb-3 mr-2 ml-8 opacity-50 mt-1"
-                    v-show="isClicked"
+                    :src="link.img1"
+                    class="rounded-full w-5 h-5 mb-3 mr-2 ml-7 mt-1"
                   />
-                  <img
-                    src="../assets/add_solid.svg"
-                    class="rounded-full w-5 h-5 mb-3 mr-2 ml-8 mt-1"
-                    v-show="!isClicked"
-                  />
-
-                  <h1
-                    class="text-white opacity-50 font-customFont text-lg"
-                    v-show="isClicked"
-                  >
-                    Today
-                  </h1>
-                  <h1
-                    class="text-white font-customFont text-lg"
-                    v-show="!isClicked"
-                  >
-                    Today
-                  </h1>
-                </span>
-              </router-link>
-            </li>
-            <li
-              class="mb-2"
-              v-on:click="
-                (isClicked = true),
-                  (isClicked2 = false),
-                  (isClicked3 = true),
-                  (isClicked4 = true),
-                  (isClicked5 = true)
-              "
-            >
-              <router-link :to="{ name: 'Projects' }">
-                <span class="flex">
-                  <img
-                    src="../assets/project.svg"
-                    class="rounded-full w-5 h-5 mb-3 opacity-50 mr-2 ml-8 mt-1"
-                    v-show="isClicked2"
-                  />
-                  <img
-                    src="../assets/project_solid.svg"
-                    class="rounded-full w-5 h-5 mb-3 mr-2 ml-8 mt-1"
-                    v-show="!isClicked2"
-                  />
-                  <h1
-                    class="text-white opacity-50 font-customFont text-lg"
-                    v-show="isClicked2"
-                  >
-                    Projects
-                  </h1>
-                  <h1
-                    class="text-white font-customFont text-lg"
-                    v-show="!isClicked2"
-                  >
-                    Projects
-                  </h1>
-                </span>
-              </router-link>
-            </li>
-            <li
-              class="mb-2"
-              v-on:click="
-                (isClicked = true),
-                  (isClicked2 = true),
-                  (isClicked3 = false),
-                  (isClicked4 = true),
-                  (isClicked5 = true)
-              "
-            >
-              <router-link :to="{ name: 'Clients' }">
-                <span class="flex">
-                  <img
-                    src="../assets/client.svg"
-                    class="rounded-full w-5 h-5 mb-3 mr-2 ml-8 opacity-50 mt-1"
-                    v-show="isClicked3"
-                  />
-                  <img
-                    src="../assets/client_solid.svg"
-                    class="rounded-full w-5 h-5 mb-3 mr-2 ml-8 mt-1"
-                    v-show="!isClicked3"
-                  />
-                  <h1
-                    class="text-white opacity-50 font-customFont text-lg"
-                    v-show="isClicked3"
-                  >
-                    Clients
-                  </h1>
-                  <h1
-                    class="text-white font-customFont text-lg"
-                    v-show="!isClicked3"
-                  >
-                    Clients
-                  </h1>
-                </span>
-              </router-link>
-            </li>
-            <li
-              v-on:click="
-                (isClicked = true),
-                  (isClicked2 = true),
-                  (isClicked3 = true),
-                  (isClicked4 = false),
-                  (isClicked5 = true)
-              "
-            >
-              <router-link :to="{ name: 'Calendar' }">
-                <span class="flex">
-                  <img
-                    src="../assets/calendar.svg"
-                    class="rounded-full w-5 h-5 mb-3 mr-2 ml-8 opacity-50 mt-1"
-                    v-show="isClicked4"
-                  />
-                  <img
-                    src="../assets/calendar_solid.svg"
-                    class="rounded-full w-5 h-5 mb-3 mr-2 ml-8 mt-1"
-                    v-show="!isClicked4"
-                  />
-                  <h1
-                    class="text-white opacity-50 font-customFont text-lg"
-                    v-show="isClicked4"
-                  >
-                    Calendar
-                  </h1>
-                  <h1
-                    class="text-white font-customFont text-lg"
-                    v-show="!isClicked4"
-                  >
-                    Calendar
+                  <h1 class="text-white font-customFont text-lg">
+                    {{ link.name }}
                   </h1>
                 </span>
               </router-link>
@@ -317,54 +70,52 @@
           </ul>
         </div>
       </div>
-      <div
-        class="mb-5 mr-40"
-        v-on:click="
-          (isClicked = true),
-            (isClicked2 = true),
-            (isClicked3 = true),
-            (isClicked4 = true),
-            (isClicked5 = false)
-        "
-      >
-        <router-link :to="{ name: 'Settings' }">
-          <span class="flex">
-            <img
-              src="../assets/settings.svg"
-              class="rounded-full w-5 h-5 mb-3 mr-2 ml-8 opacity-50 mt-1"
-              v-show="isClicked5"
-            />
-            <img
-              src="../assets/settings_solid.svg"
-              class="rounded-full w-5 h-5 mb-3 mr-2 ml-8 mt-1"
-              v-show="!isClicked5"
-            />
-            <h1
-              class="text-white opacity-50 font-customFont text-lg"
-              v-show="isClicked5"
-            >
-              Settings
-            </h1>
-            <h1 class="text-white font-customFont text-lg" v-show="!isClicked5">
-              Settings
-            </h1>
-          </span>
-        </router-link>
-      </div>
     </nav>
   </div>
 </template>
 
 <script>
 export default {
-  data: () => {
+  data() {
     return {
-      isClicked: true,
-      isClicked2: true,
-      isClicked3: true,
-      isClicked4: true,
-      isClicked5: true,
       arrow: true,
+      links: [
+        {
+          id: 1,
+          name: "Today",
+          img1: require("../assets/add.svg"),
+          img2: require("../assets/add_solid.svg"),
+          route: "Add",
+        },
+        {
+          id: 2,
+          name: "Projects",
+          img1: require("../assets/project.svg"),
+          img2: require("../assets/project_solid.svg"),
+          route: "Projects",
+        },
+        {
+          id: 3,
+          name: "Clinets",
+          img1: require("../assets/client.svg"),
+          img2: require("../assets/client_solid.svg"),
+          route: "Clients",
+        },
+        {
+          id: 4,
+          name: "Calendar",
+          img1: require("../assets/calendar.svg"),
+          img2: require("../assets/calendar_solid.svg"),
+          route: "Calendar",
+        },
+        {
+          id: 5,
+          name: "Settings",
+          img1: require("../assets/settings.svg"),
+          img2: require("../assets/settings_solid.svg"),
+          route: "Settings",
+        },
+      ],
     };
   },
 };
