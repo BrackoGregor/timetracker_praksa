a5d1a6862bcb6dbc21aad2f92b49cc4d8f2a82ef
tomas <tomas.kovacic1@gmail.com>
2021-04-09 11:46:28 +0200

    added search bar in ExtendedNavbar

--- web/src/components/ExtendedNavbar.vue
+++ web/src/components/ExtendedNavbar.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="flex flex-row h-screen shadow-sideBar">
-    <nav class="bg-blue-light w-40 justify-between flex flex-col">
-      <div class="mt-5 mb-10"></div>
+    <nav class="bg-blue-light w-60 justify-between items-center flex flex-col">
+      <div class="mt-5 mb-10">
+          <div class="flex">
+              <img src="../assets/search.svg" class="h-5 w-5 mt-1">
+              <input type="text" placeholder="search for clients" class="w-40 bg-blue-light text-center text-white placeholder-gray-100::placeholder">
+          </div>
+      </div>
     </nav>
   </div>
 </template>
