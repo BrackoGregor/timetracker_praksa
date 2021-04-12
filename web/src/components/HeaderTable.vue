b342dc3be35405cd4dcfe53aae946690ad5d333a
tomas <tomas.kovacic1@gmail.com>
2021-04-12 13:08:54 +0200

    Added header table for settings and calendar

--- web/src/components/HeaderTable.vue
+++ web/src/components/HeaderTable.vue
@@ -3,13 +3,19 @@
     class="flex bg-blue h-12 items-center justify-center mx-3 my-3 rounded-md shadow-headerTable"
   >
     <h1 class="font-customFont font-medium text-white text-2xl">
-      Solve-x d.o.o.
+      {{ currentRouteName }}
     </h1>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    currentRouteName() {
+      return this.$route.name.trim();
+    },
+  },
+};
 </script>
 
 <style>
