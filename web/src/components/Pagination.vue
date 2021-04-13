b7afd1d5aca0c50cc5b0bfe61fdaf70fa4296c0f
“ypeed” <patra.kaca@gmail.com>
2021-04-13 11:50:18 +0200

    added paggination view

---
+++ web/src/components/Pagination.vue
@@ -0,0 +1,182 @@
+<template>
+  <ul class="flex flex-row font-customFont items-center">
+    <li class="h-6">
+      <button type="button" @click="onClickFirstPage" :disabled="isInFirstPage">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+          />
+        </svg>
+      </button>
+    </li>
+
+    <li class="px-2 h-6">
+      <button
+        type="button"
+        @click="onClickPreviousPage"
+        :disabled="isInFirstPage"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 19l-7-7 7-7"
+          />
+        </svg>
+      </button>
+    </li>
+
+    <li v-for="page in pages">
+      <button
+        type="button"
+        @click="onClickPage(page.name)"
+        :disabled="page.isDisabled"
+        class="text-xl p-3 font-medium"
+      >
+        {{ page.name }}
+      </button>
+    </li>
+
+    <li class="px-2 h-6">
+      <button type="button" @click="onClickNextPage" :disabled="isInLastPage">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 5l7 7-7 7"
+          />
+        </svg>
+      </button>
+    </li>
+
+    <li class="h-6">
+      <button type="button" @click="onClickLastPage" :disabled="isInLastPage">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 5l7 7-7 7M5 5l7 7-7 7"
+          />
+        </svg>
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    maxVisibleButtons: {
+      type: Number,
+      required: false,
+      default: 3,
+    },
+    totalPages: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    perPage: {
+      type: Number,
+      required: true,
+    },
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    startPage() {
+      if (this.currentPage === 1) {
+        return 1;
+      }
+
+      if (this.currentPage === this.totalPages) {
+        return this.totalPages - this.maxVisibleButtons + 1;
+      }
+
+      return this.currentPage - 1;
+    },
+    endPage() {
+      return Math.min(
+        this.startPage + this.maxVisibleButtons - 1,
+        this.totalPages
+      );
+    },
+    pages() {
+      const range = [];
+
+      for (let i = this.startPage; i <= this.endPage; i += 1) {
+        range.push({
+          name: i,
+          isDisabled: i === this.currentPage,
+        });
+      }
+
+      return range;
+    },
+    isInFirstPage() {
+      return this.currentPage === 1;
+    },
+    isInLastPage() {
+      return this.currentPage === this.totalPages;
+    },
+  },
+  methods: {
+    onClickFirstPage() {
+      this.$emit("pagechanged", 1);
+    },
+    onClickPreviousPage() {
+      this.$emit("pagechanged", this.currentPage - 1);
+    },
+    onClickPage(page) {
+      this.$emit("pagechanged", page);
+    },
+    onClickNextPage() {
+      this.$emit("pagechanged", this.currentPage + 1);
+    },
+    onClickLastPage() {
+      this.$emit("pagechanged", this.totalPages);
+    },
+    isPageActive(page) {
+      return this.currentPage === page;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
