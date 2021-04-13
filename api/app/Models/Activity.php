79008cd1e60e014e88ca47700acfa4b0aea1abe2
Gregor Bračko <brackog@gmail.com>
2021-04-13 18:09:04 +0200

    Update Activity
    Removed protected $guarded,
    used SoftDeletes,
    added protected filed 'deleted_at',
    added fillable method,
    added foreign key method

--- api/app/Models/Activity.php
+++ api/app/Models/Activity.php
@@ -4,9 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Activity extends Model
 {
-    protected array $guarded = [];
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'title',
+        'start_time',
+        'end_time',
+        'comment',
+        'id_assignments'
+    ];
+
+    public function assignment(): HasMany
+    {
+        return $this->hasMany(Assignment::class, 'id_assignments', 'id');
+    }
+
     use HasFactory;
 }
+
