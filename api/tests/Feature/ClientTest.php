f91a6317f2d9bf406b13fddfcbd356c8378796de
Gregor Bračko <brackog@gmail.com>
2021-04-13 18:03:57 +0200

    Update ClientTest
    Added authorization to test methods

--- api/tests/Feature/ClientTest.php
+++ api/tests/Feature/ClientTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Passport\Passport;
 use Tests\TestCase;
 use App\Models\Client;
 
@@ -11,6 +13,9 @@ class ClientTest extends TestCase
 {
     public function test_client_can_create()
     {
+        $user = User::factory()->create();
+        Passport::actingAs($user);
+
         $createdClient = Client::factory()->make();
         $response = $this->post('/api/v1/clients', $createdClient->toArray());
         $response->assertStatus(201);
@@ -19,6 +24,9 @@ class ClientTest extends TestCase
 
     public function test_client_can_delete()
     {
+        $user = User::factory()->create();
+        Passport::actingAs($user);
+
         $createdClient = Client::factory()->create();
         $response = $this->delete('/api/v1/clients/'. $createdClient->id);
         $response->assertStatus(204);
@@ -27,6 +35,9 @@ class ClientTest extends TestCase
 
     public function test_client_can_update()
     {
+        $user = User::factory()->create();
+        Passport::actingAs($user);
+
         $createdClientInDb = Client::factory()->create();
         $createdClient = Client::factory()->make();
         $response = $this->patch('/api/v1/clients/'. $createdClientInDb->id, $createdClient->toArray());
@@ -36,6 +47,9 @@ class ClientTest extends TestCase
 
     public function test_client_can_get()
     {
+        $user = User::factory()->create();
+        Passport::actingAs($user);
+
         $createdClientInDb = Client::factory()->create();
         $response = $this->get('/api/v1/clients/'. $createdClientInDb->id);
         $response->assertStatus(200);
@@ -44,18 +58,27 @@ class ClientTest extends TestCase
 
     public function test_client_can_get_all()
     {
+        $user = User::factory()->create();
+        Passport::actingAs($user);
+
         $response = $this->get('/api/v1/clients');
         $response->assertStatus(200);
     }
 
     public function test_client_can_not_show_client_with_invalid_id()
     {
+        $user = User::factory()->create();
+        Passport::actingAs($user);
+
         $response = $this->get('/api/v1/clients/'. 0);
         $response->assertStatus(404);
     }
 
     public function test_client_can_not_update_client_with_invalid_id()
     {
+        $user = User::factory()->create();
+        Passport::actingAs($user);
+
         $createdClient = Client::factory()->make();
         $response = $this->patch('/api/v1/clients/'. 0, $createdClient->toArray());
         $response->assertStatus(404);
@@ -63,6 +86,9 @@ class ClientTest extends TestCase
 
     public function test_client_can_not_delete_client_with_invalid_id()
     {
+        $user = User::factory()->create();
+        Passport::actingAs($user);
+
         $response = $this->delete('/api/v1/clients/'. 0);
         $response->assertStatus(404);
     }
