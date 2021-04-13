1c7953291580cdbb93a4d871194d55241844dee9
Gregor Bračko <brackog@gmail.com>
2021-04-13 18:27:29 +0200

    Update Contact
    Removed protected $guarded,
    used SoftDeletes,
    added protected filed 'deleted_at',
    added fillable method,
    added foreign key method

--- api/app/Models/Contact.php
+++ api/app/Models/Contact.php
@@ -7,6 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    protected array $guarded = [];
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'email',
+        'phone',
+        'id_client'
+    ];
+
+    public function assignment(): HasMany
+    {
+        return $this->hasMany(Assignment::class, 'id_client', 'id');
+    }
+
     use HasFactory;
 }
