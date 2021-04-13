4fa58eeb989012a704cbb009d0dbc0ff4f13b2e7
“ypeed” <patra.kaca@gmail.com>
2021-04-13 07:17:55 +0200

    hotfix

--- web/src/components/ClientTable.vue
+++ web/src/components/ClientTable.vue
@@ -1,5 +1,49 @@
 <template>
-  <div></div>
+  <div class="flex flex-col">
+    <div class="bg-blue-light mx-3 my-3 rounded-md">
+      <table class="w-full">
+        <thead>
+          <tr>
+            <th>Project</th>
+            <th>Note</th>
+            <th>Start time</th>
+            <th>End time</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="text-center">
+            <td>Sole.x</td>
+            <td>TEST</td>
+            <td>11:00</td>
+            <td>12:00</td>
+            <td>active</td>
+          </tr>
+          <tr class="text-center">
+            <td>Sole.x</td>
+            <td>TEST</td>
+            <td>11:00</td>
+            <td>12:00</td>
+            <td>active</td>
+          </tr>
+          <tr class="text-center">
+            <td>Sole.x</td>
+            <td>TEST</td>
+            <td>11:00</td>
+            <td>12:00</td>
+            <td>active</td>
+          </tr>
+        </tbody>
+      </table>
+      <div>
+        <ul class="flex flex-row justify-center">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
