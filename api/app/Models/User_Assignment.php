6c917d4cf09e2ccb852d7a6e5b39ad23b0ef7bab
Gregor Bračko <brackog@gmail.com>
2021-04-14 20:50:31 +0200

    Update User_Assignment
    Added fillable, foreign keys, softDeletes

--- api/app/Models/User_Assignment.php
+++ api/app/Models/User_Assignment.php
@@ -4,26 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User_Assignment extends Model
 {
-    public string $table = 'users_assignments';
+    use HasFactory;
 
-    protected array $fillable = [
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
         'id_users',
         'id_assignments'
     ];
 
-    use HasFactory;
-
-    public function user(): HasOne
+    public function user(): HasMany
     {
-        return $this->hasOne(User::class, 'id_users', 'id');
+        return $this->hasMany(User::class, 'id_clients', 'id');
     }
 
-    public function assignment(): HasOne
+    public function assignment(): HasMany
     {
-        return $this->hasOne(Assignment::class,'id_assignments', 'id');
+        return $this->hasMany(Assignment::class, 'id_assignments', 'id');
     }
 }
