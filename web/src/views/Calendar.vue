e49d2c85b9e2372a1e54bcc7686f8ad8253dff63
tomas <tomas.kovacic1@gmail.com>
2021-04-15 10:48:36 +0200

    Calendar event test added

--- web/src/views/Calendar.vue
+++ web/src/views/Calendar.vue
@@ -5,7 +5,17 @@
       <div
         class="flex bg-white h-full items-center justify-center mx-3 mb-3 rounded-md shadow-headerTable"
       >
-        <vue-cal :time="false" hide-weekends active-view="week" :disable-views="['years']" class="font-customFont"/>
+        <vue-cal
+          :events="events"
+          :timeCellHeight="31"
+          :time-from="7 * 60"
+          :time-to="15 * 60"
+          :time-step="30"
+          hide-weekends
+          active-view="week"
+          :disable-views="['years']"
+          class="font-customFont"
+        />
       </div>
     </div>
   </div>
@@ -21,18 +31,52 @@ export default {
     HeaderTable,
     VueCal,
   },
+  data: () => ({
+    events: [
+      {
+        start: "2021-4-15 11:00",
+        end: "2021-4-15 11:30",
+        title: "Sestanek",
+        content: '<i>Sestanek</i>',
+        class: "mark",
+      },
+    ],
+  }),
 };
 </script>
 
 <style>
-.vuecal__menu, .vuecal__cell-events-count {background-color: #032A4E;color: white;}
-.vuecal__title-bar {background-color: #03779E; color:white}
-.vuecal__cell--today, .vuecal__cell--current {background-color: rgba(198, 198, 218, 0.4);}
-.vuecal:not(.vuecal--day-view) .vuecal__cell--selected {background-color: rgba(211, 218, 243, 0.4);}
-.vuecal__cell--selected:before {border-color: #03779E;}
+.vuecal__menu,
+.vuecal__cell-events-count {
+  background-color: #032a4e;
+  color: white;
+}
+.vuecal__title-bar {
+  background-color: #03779e;
+  color: white;
+}
+.vuecal__cell--today,
+.vuecal__cell--current {
+  background-color: rgba(198, 198, 218, 0.4);
+}
+.vuecal:not(.vuecal--day-view) .vuecal__cell--selected {
+  background-color: rgba(211, 218, 243, 0.4);
+}
+.vuecal__cell--selected:before {
+  border-color: #03779e;
+}
 .vuecal__cell--highlighted:not(.vuecal__cell--has-splits),
-.vuecal__cell-split--highlighted {background-color:#03779E}
+.vuecal__cell-split--highlighted {
+  background-color: #03779e;
+}
 .vuecal__arrow.vuecal__arrow--highlighted,
-.vuecal__view-btn.vuecal__view-btn--highlighted {background-color:#03779E;}
+.vuecal__view-btn.vuecal__view-btn--highlighted {
+  background-color: #03779e;
+}
 
+.vuecal__event.mark {
+  background-color: #03779e;
+  border: 1px solid #032a4e;
+  color: #fff;
+}
 </style>
