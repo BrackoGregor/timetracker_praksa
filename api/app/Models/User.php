72f18f5919a5d89c552e641016d9454da2401dc4
Samo Faric <samofaric99@gmail.com>
2021-04-28 09:54:28 +0200

    Update User.php
    Added hash password

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
@@ -31,17 +31,13 @@ class User extends Authenticatable
         'id_users_roles'
     ];
 
-    public function roles(): HasOne
-    {
-        return $this->hasOne(User_Role::class, 'id_users_roles', 'id');
-    }
-
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
     protected $hidden = [
+        'password',
         'remember_token',
     ];
 
@@ -54,4 +50,17 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function setPasswordAttribute($password)
+    {
+        if (trim($password) === '') {
+            return;
+        }
+        $this->attributes['password'] = Hash::make($password);
+    }
+
+
+    public function user_role(): HasOne
+    {
+        return $this->hasOne(User_Role::class,'id_users_roles', 'id');
+    }
 }
