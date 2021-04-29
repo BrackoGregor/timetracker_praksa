d43cbd0c7f26db435a82218fd4c74366fe6d54e7
tomas <tomas.kovacic1@gmail.com>
2021-04-29 14:08:15 +0200

    Adding subscribers file to the store for easier authorization

--- web/src/main.js
+++ web/src/main.js
@@ -7,6 +7,8 @@ import store from "./store";
 import axios from "axios";
 import "./index.css";
 
+require('@/store/subscriber');
+
 axios.defaults.baseURL = "http://127.0.0.1:8000"
 
 createApp(App).use(store).use(router).mount("#app");
